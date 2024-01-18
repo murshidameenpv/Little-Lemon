4e8b0322424444d99ede917927b80724a931a9a8
murshidameen <murshidmonzpv@gmail.com>
2024-01-19 03:24:34 +0530

    responsive Navbar ui completed,mediaquery

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import logo from '../images/Logo .svg'
+function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false)
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    }
+  return (
+      <nav className={`navbar ${menuOpen?"open":""}`}>
+          <a href='/' className='logo'>
+              <img src={logo} alt="logo" />
+          </a>
+          {/* mobile navbar */}
+          <div className='menu-icon' onClick={toggleMenu}>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+          </div>
+          {/* {nav itemes} */}
+          <ul className={`nav-links ${menuOpen?"visible":""}`}>
+              <li>
+                  <a href="/">Home</a>
+              </li>
+              <li>
+                  <a href="/">About</a>
+              </li>
+              <li>
+                  <a href="/">Services</a>
+              </li>
+              <li>
+                  <a href="/">Menu</a>
+              </li>
+              <li>
+                  <a href="/">Reservation</a>
+              </li>
+              <li>
+                  <a href="/">Order Online</a>
+              </li>
+              <li>
+                  <a href="/">Login</a>
+              </li>
+          </ul>
+    </nav>
+  )
+}
+
+export default Navbar
